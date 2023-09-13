cfc62c27d66dc665ef0782efac24c1cef1b5d221
Brandon Trump <brandontrump101@gmail.com>
2023-09-13 21:41:18 +1200

    chore: included background images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,53 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "header-mobile": "url('/images/mobile/image-header.jpg')",
+        "header-desktop": "url('/images/desktop/image-header.jpg')",
+        "egg-desktop": "url('/images/desktop/image-transform.jpg')",
+        "egg-mobile": "url('/images/mobile/image-transform.jpg')",
+        "stand-out-desktop": "url('/images/desktop/image-stand-out.jpg')",
+        "stand-out-mobile": "url('/images/mobile/image-stand-out.jpg')",
+        "cherry-desktop": "url('/images/desktop/image-graphic-design.jpg')",
+        "cherry-mobile": "url('/images/mobile/image-graphic-design.jpg')",
+        "orange-desktop": "url('/images/desktop/image-photography.jpg')",
+        "orange-mobile": "url('/images/mobile/image-photography.jpg')",
+        "cone-desktop": "url('/images/desktop/image-gallery-cone.jpg')",
+        "milkbottles-desktop":
+          "url('/images/desktop/image-gallery-milkbottles.jpg')",
+        "gallery-orange-desktop":
+          "url('/images/desktop/image-gallery-orange.jpg')",
+        "sugar-desktop": "url('/images/desktop/image-gallery-sugarcubes.jpg')",
+      },
+      fontFamily: {
+        barlow: ["var(--font-barlow)"],
+        fraunces: ["var(--font-fraunces)"],
+      },
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1440px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
